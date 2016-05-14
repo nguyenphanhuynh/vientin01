229f9ce9666dfc51029b68a8346d1c75a80b9728
Huynh Nguyen <nguyenphanhuynh@gmail.com>
2016-05-14 14:15:57 +0700

    Init project

---
+++ protected/views/users/_form.php
@@ -0,0 +1,76 @@
+<?php
+/* @var $this UsersController */
+/* @var $model User */
+/* @var $form CActiveForm */
+?>
+
+<div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'users-form',
+	// Please note: When you enable ajax validation, make sure the corresponding
+	// controller action is handling ajax validation correctly.
+	// There is a call to performAjaxValidation() commented in generated controller code.
+	// See class documentation of CActiveForm for details on this.
+	'enableAjaxValidation'=>false,
+)); ?>
+
+	<p class="note">Fields with <span class="required">*</span> are required.</p>
+
+	<?php echo $form->errorSummary($model); ?>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'username'); ?>
+		<?php echo $form->textField($model,'username',array('size'=>60,'maxlength'=>128)); ?>
+		<?php echo $form->error($model,'username'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'password'); ?>
+		<?php echo $form->passwordField($model,'password',array('size'=>60,'maxlength'=>512)); ?>
+		<?php echo $form->error($model,'password'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'gender'); ?>
+		<?php echo $form->textField($model,'gender',array('size'=>45,'maxlength'=>45)); ?>
+		<?php echo $form->error($model,'gender'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'dob'); ?>
+		<?php echo $form->textField($model,'dob'); ?>
+		<?php echo $form->error($model,'dob'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'full_name'); ?>
+		<?php echo $form->textField($model,'full_name',array('size'=>60,'maxlength'=>256)); ?>
+		<?php echo $form->error($model,'full_name'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'address'); ?>
+		<?php echo $form->textField($model,'address',array('size'=>60,'maxlength'=>512)); ?>
+		<?php echo $form->error($model,'address'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'email'); ?>
+		<?php echo $form->textField($model,'email',array('size'=>60,'maxlength'=>256)); ?>
+		<?php echo $form->error($model,'email'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'avatar'); ?>
+		<?php echo $form->textField($model,'avatar',array('size'=>60,'maxlength'=>256)); ?>
+		<?php echo $form->error($model,'avatar'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- form -->
---
+++ protected/views/users/_view.php
@@ -0,0 +1,47 @@
+<?php
+/* @var $this UsersController */
+/* @var $data User */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('username')); ?>:</b>
+	<?php echo CHtml::encode($data->username); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('password')); ?>:</b>
+	<?php echo CHtml::encode($data->password); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('gender')); ?>:</b>
+	<?php echo CHtml::encode($data->gender); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('dob')); ?>:</b>
+	<?php echo CHtml::encode($data->dob); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('full_name')); ?>:</b>
+	<?php echo CHtml::encode($data->full_name); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('address')); ?>:</b>
+	<?php echo CHtml::encode($data->address); ?>
+	<br />
+
+	<?php /*
+	<b><?php echo CHtml::encode($data->getAttributeLabel('email')); ?>:</b>
+	<?php echo CHtml::encode($data->email); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('avatar')); ?>:</b>
+	<?php echo CHtml::encode($data->avatar); ?>
+	<br />
+
+	*/ ?>
+
+</div>
